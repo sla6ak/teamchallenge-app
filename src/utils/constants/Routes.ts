84dd7f3802e531dd82fdee1851233216b9a3853d
sla6ak <slabakxaker@gmail.com>
2024-06-04 21:37:15 +0300

    add pages

--- src/utils/constants/Routes.ts
+++ src/utils/constants/Routes.ts
@@ -1,9 +1,10 @@
 export const ROUTES = {
   Home: { title: 'Головна', link: 'home' },
-  Ocasions: { title: 'мій профіль', link: 'ocasions' },
+  Profile: { title: 'мій профіль', link: 'profile' },
   Likes: { title: 'улюблені', link: 'likes' },
   ShoppingCart: { title: 'кошик', link: 'shoppingcart' },
   Flowers: { title: 'Квіти', link: 'flowers' },
   Bouquets: { title: 'Букети', link: 'bouquets' },
-  GiftSets: { title: 'Подарункові набори', link: 'giftsets' }
+  GiftSets: { title: 'Подарункові набори', link: 'giftsets' },
+  Auth: { title: 'Авторизація ', link: 'auth' }
 }
